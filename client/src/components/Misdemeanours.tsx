9cb79c26cf9d79fe3919d8246a681e2c259832a5
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-09-20 12:07:46 +0100

    Feat: added filter for misdemeanours

--- client/src/components/Misdemeanours.tsx
+++ client/src/components/Misdemeanours.tsx
@@ -1,21 +1,41 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Misdemeanour } from "../../types/misdemeanours.types";
 import { emojies } from "../../types/misdemeanours.types";
 
 const Misdemeanours: React.FC = () => {
   const [cases, setCases] = useState<Array<Misdemeanour>>();
+  const [selectedMisdemeanour, setSelectedMisdemeanour] = useState("");
+
+  const AMOUNT = 5;
 
   useEffect(() => {
     const getCases = async () => {
-      const data = await fetch("http://localhost:8080/api/misdemeanours/3");
+      const data = await fetch(
+        `http://localhost:8080/api/misdemeanours/${AMOUNT}`
+      );
       data.json().then((json) => {
         setCases(json.misdemeanours);
       });
     };
     getCases();
   }, []);
+
   return (
     <>
+      <div>
+        <label>Filter by Misdemeanour:</label>
+        <select
+          value={selectedMisdemeanour}
+          onChange={(e) => setSelectedMisdemeanour(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="rudeness">Mild Public Rudeness</option>
+          <option value="lift">Speaking in a Lift</option>
+          <option value="vegetables">Not Eating Your Vegetables</option>
+          <option value="united">Supporting Manchester United</option>
+        </select>
+      </div>
+
       <p>{cases === undefined && "Loading..."}</p>
       {cases && (
         <table>
@@ -26,21 +46,27 @@ const Misdemeanours: React.FC = () => {
               <th>Misdemeanour</th>
               <th>Punishment idea</th>
             </tr>
-            {cases.map((caseItem) => (
-              <tr key={caseItem.citizenId}>
-                <td>{caseItem.citizenId}</td>
-                <td>{caseItem.date}</td>
-                <td>
-                  {emojies[caseItem.misdemeanour]} {caseItem.misdemeanour}
-                </td>
-                <td>
-                  <img
-                    src={`https://picsum.photos/190/100?random=${Math.random()}`}
-                    alt="punishment idea"
-                  />
-                </td>
-              </tr>
-            ))}
+            {cases
+              .filter((caseItem) =>
+                selectedMisdemeanour
+                  ? caseItem.misdemeanour === selectedMisdemeanour
+                  : true
+              )
+              .map((caseItem) => (
+                <tr key={caseItem.citizenId}>
+                  <td>{caseItem.citizenId}</td>
+                  <td>{caseItem.date}</td>
+                  <td>
+                    {emojies[caseItem.misdemeanour]} {caseItem.misdemeanour}
+                  </td>
+                  <td>
+                    <img
+                      src={`https://picsum.photos/190/100?random=${Math.random()}`}
+                      alt="punishment idea"
+                    />
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       )}
